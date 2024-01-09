007c99e686ede34f309d910ca268738539c643aa
StasBd <stasbodnaruk71@gmail.com>
2024-01-09 23:00:12 +0200

    Fixed Bottom Navigation, new fish types, and modifications coming soon!

--- app/src/main/java/com/example/fishaccompanyapp/MainActivity.kt
+++ app/src/main/java/com/example/fishaccompanyapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.example.fishaccompanyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,38 +17,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.continentsBtnNavView.setOnNavigationItemReselectedListener{
-            true
-            when(it.itemId){
+        //function to change fragments
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.europe -> replaceFragment(EuropeFragment())
+                else -> {
 
-                R.id.europe -> {}
-
-                R.id.asia -> {}
-
-                R.id.africa -> {}
-
-                R.id.america -> {}
+                }
             }
+            true
         }
     }
 
-  // logic of button karas
-    fun onClickKaras(view: View){
-        val intent = Intent(this, KarasActivity::class.java)
-        startActivityForResult(intent, 1)
-        var SendDATA = intent.putExtra("KarasInfo", 1)
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
 
     }
-  // logic of button karp
-    fun onClickKarp(view: View){
-        val intent = Intent(this, KarpActivity::class.java)
-        startActivityForResult(intent, 2)
-    }
-    // logic of Okun
-    fun onClickOkun(view: View){
-        val intent = Intent(this, OkunActivity::class.java)
-        startActivityForResult(intent, 3)
-    }
-
-
 }
