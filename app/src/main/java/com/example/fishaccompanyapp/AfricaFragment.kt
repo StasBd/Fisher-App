c58fc072650529839c904c66f42af939ff8d88a0
StasBd <stasbodnaruk71@gmail.com>
2024-01-19 22:22:59 +0200

    Added new fish, right now coming more! And as I say, mega refactor preparing for this app:3

--- app/src/main/java/com/example/fishaccompanyapp/AfricaFragment.kt
+++ app/src/main/java/com/example/fishaccompanyapp/AfricaFragment.kt
@@ -1,15 +1,23 @@
 package com.example.fishaccompanyapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fishaccompanyapp.databinding.FragmentAfricaBinding
+import com.example.fishaccompanyapp.databinding.FragmentAmericaBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+
+private var _binding: FragmentAfricaBinding? = null
+private val binding get() = _binding!!
+
 
 /**
  * A simple [Fragment] subclass.
@@ -32,9 +40,19 @@ class AfricaFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_africa, container, false)
+        _binding = FragmentAfricaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.belonophagoButton.setOnClickListener{
+            val intent = Intent(activity, BelonophagoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     companion object {
