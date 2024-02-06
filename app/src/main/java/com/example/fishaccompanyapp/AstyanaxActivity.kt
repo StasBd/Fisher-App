7d3af8ad3ed33cf8f7d772b14b48ef5337ba2622
StasBd <stasbodnaruk71@gmail.com>
2024-02-06 18:55:51 +0200

    Added Reusable activity class, now I'm trying to realize logic to make it working...

---
+++ app/src/main/java/com/example/fishaccompanyapp/AstyanaxActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fishaccompanyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AstyanaxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_astyanax)
+    }
+}
---
+++ app/src/main/java/com/example/fishaccompanyapp/ReusableActivity.kt
@@ -0,0 +1,7 @@
+package com.example.fishaccompanyapp
+
+
+class ReusableActivity(Name: String, Age: Int, Bio: String ){
+    // STILL LOOKING FOR WAY TO MAKE IT WORKING....
+
+}
